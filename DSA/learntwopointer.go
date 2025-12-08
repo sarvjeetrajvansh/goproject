1cf94f4e6fd4a5ba680a8a29fcaccd56dd6cbd9a
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2025-12-08 20:22:17 +0530

    Two Pointers

--- DSA/learntwopointer.go
+++ DSA/learntwopointer.go
@@ -15,8 +15,25 @@ func isPalindrome(arr []int) bool {
 	return true
 
 }
+func hasPairWithSum(arr []int, target int) bool {
+	left, right := 0, len(arr)-1
+	for left < right {
+		sum := arr[left] + arr[right]
+		if sum == target {
+			return true
+		}
+		if sum < target {
+			left++
+		} else {
+			right--
+		}
+
+	}
+	return false
+}
 
 func main() {
 	fmt.Println(isPalindrome([]int{1, 2, 3, 2, 1}))
 	fmt.Println(isPalindrome([]int{1, 2, 3, 4, 5}))
+	fmt.Println(hasPairWithSum([]int{1, 2, 3, 4, 5}, 5))
 }
