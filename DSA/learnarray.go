263c098a29445e14dba537a5d70763d7abb27715
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2025-12-13 15:45:30 +0530

    DSA dsy

--- DSA/learnarray.go
+++ DSA/learnarray.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// two pointer
 func isPalindrome(arr []int) bool {
 	left, right := 0, len(arr)-1
 
@@ -37,17 +38,41 @@ func removeDuplicates(arr []int) int {
 	}
 	l := 0
 	for r := 1; r < len(arr); r++ {
-		if arr[l] != arr[r] { //
+		if arr[l] != arr[r] {
 			l++
-			arr[l] = arr[r] //
+			arr[l] = arr[r]
 		}
 	}
 	return l + 1
 }
+func maxWaterContainer(height []int) int {
+	return 0
+}
 
+// optimal largest element
+func largestElement(nums []int) int {
+	maxElement := nums[0]
+	for _, value := range nums {
+		if maxElement < value {
+			maxElement = value
+		}
+	}
+	return maxElement
+}
+func linearSearch(nums []int, target int) int {
+	for index, value := range nums {
+		if value == target {
+			return index
+		}
+	}
+	return -1
+}
 func main() {
 	fmt.Println(isPalindrome([]int{1, 2, 3, 2, 1}))
 	fmt.Println(isPalindrome([]int{1, 2, 3, 4, 5}))
 	fmt.Println(hasPairWithSum([]int{1, 2, 3, 4, 5}, 5))
 	fmt.Println(removeDuplicates([]int{1, 2, 3, 2, 1}))
+	fmt.Println(maxWaterContainer([]int{1, 2, 3, 2, 1}))
+	fmt.Println(largestElement([]int{1, 2, 3, 2, 11}))
+	fmt.Println(linearSearch([]int{1, 2, 3, 4, 5}, 5))
 }
