63d0a859f940135ae3dd35a732c0f8dadd1743a9
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2025-12-25 22:31:47 +0530

    Get Response

--- Projects/app.go
+++ Projects/app.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
 func main() {
 
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(body))
 }
